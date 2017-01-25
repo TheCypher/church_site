4b7f9fd9c6b1c5d3b77bffe5dd5176911aba4039
nicksonthecypher <nicksonthecypher@gmail.com>
2017-01-24 23:40:46 -0800

    Admin->[dashboard, websites, edit-page]

--- app/admin/views.py
+++ app/admin/views.py
@@ -18,6 +18,12 @@ def index():
 	}
 	return render_template('admin/index.html', page=page)
 
+@mod.route('/logout', methods=['GET'])
+@login_required
+def logout():
+	session['user_id'] = ''
+	return redirect(url_for('front.signin'))
+
 @mod.route('/website', methods=['GET', 'POST'])
 @login_required
 def website():
@@ -26,8 +32,10 @@ def website():
 	}
 	return render_template('admin/website.html', page=page)
 
-@mod.route('/logout', methods=['GET'])
-@login_required
-def logout():
-	session['user_id'] = ''
-	return redirect(url_for('front.signin'))
+@mod.route('/edit-page', methods=['GET', 'POST'])
+def edit_page():
+	pageName = 'Homepage'
+	page = {
+		'title':'Edit - %s' % (pageName)
+	}
+	return render_template('admin/edit-page.html', page=page)
